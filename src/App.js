ea9c4c97464fdd1541d76ac3d9a9d4b050cdfd24
faizan <falam9425@gmail.com>
2022-07-20 21:08:00 +0530

    code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+     <Exhibit/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Banner () {
+    return (
+        <React.Fragment>
+            <h4>Design of Sextant</h4>
+        </React.Fragment>
+
+    );
+}
+
+export default Banner;
